83e87ac19d94ab1b3ba281628032ede68e503d06
JavaEE <mikeflea19@gmail.com>
2017-11-25 09:45:34 +0200

    add RestController annotation

--- src/main/java/hillelee/pet/PetController.java
+++ src/main/java/hillelee/pet/PetController.java
@@ -1,10 +1,13 @@
 package hillelee.pet;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by JavaEE on 25.11.2017.
  */
+
+@RestController
 public class PetController {
 
     @GetMapping(value = "/greeting")
