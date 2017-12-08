2046858c0403f6cf99be19d0d8d61243c028bc7b
Oleh Cherniavskyi <olehcherniavskyi@gmail.com>
2017-12-08 20:44:18 +0200

    added Yaml specs file and YamlReaderClass

--- src/main/java/hillelee/doctor/DoctorController.java
+++ src/main/java/hillelee/doctor/DoctorController.java
@@ -3,13 +3,17 @@ package hillelee.doctor;
 
 import hillelee.pet.ErrorBody;
 import hillelee.util.NoSuchDoctorException;
+import hillelee.util.YamlReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -19,7 +23,7 @@ public class DoctorController {
     private final DoctorService doctorService;
 
     @GetMapping("/doctors/{id}")
-    public ResponseEntity<?> findDoctorById(@PathVariable Integer id) {
+    public ResponseEntity<?> findDoctorById(@PathVariable String id) {
         Optional<Doctor> maybeDoctor = doctorService.getById(id);
 
         return maybeDoctor.map(Object.class::cast)
@@ -44,7 +48,7 @@ public class DoctorController {
 
     @PutMapping("/doctors/{id}")
     public void updateADoctor(@RequestBody Doctor doctor,
-                              @PathVariable Integer id) {
+                              @PathVariable String id) {
         doctor.setId(id);
         doctorService.save(doctor);
     }
@@ -52,8 +56,13 @@ public class DoctorController {
 
     @DeleteMapping("/doctors/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteADoctor(@PathVariable Integer id) {
+    public void deleteADoctor(@PathVariable String id) {
         doctorService.delete(id).orElseThrow(NoSuchDoctorException::new);
+    }
+
+    @GetMapping("/doctors/specializations")
+    public Map<String, Object> getAllSpecializations(){
+        return doctorService.getSpecs();
     }
 
 }
--- src/main/java/hillelee/doctor/DoctorRepository.java
+++ src/main/java/hillelee/doctor/DoctorRepository.java
@@ -3,35 +3,36 @@ package hillelee.doctor;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class DoctorRepository {
     private List<Doctor> doctors = Collections.synchronizedList(new ArrayList<Doctor>());
 
-    private AtomicInteger counter = new AtomicInteger(0);
 
     public Collection<Doctor> findAll() {
         return doctors;
     }
 
-    public Optional<Doctor> findById(Integer id) {
-        return Optional.ofNullable(doctors.get(id));
+    public Optional<Doctor> findById(String id) {
+        return Optional.ofNullable(doctors.stream()
+                .filter(doctor -> doctor.getId().equals(id))
+                .findFirst()
+                .get());
     }
 
     public Doctor save(Doctor doctor) {
-        if (doctor.getId() == null) {
-            doctor.setId(counter.incrementAndGet());
+        if (doctor.getId().isEmpty()) {
+            doctor.setId(UUID.randomUUID().toString().replaceAll("-",""));
         }
         doctors.add(doctor);
 
         return doctor;
     }
 
-    public Optional<Doctor> delete(Integer id) {
+    public Optional<Doctor> delete(String id) {
 
         Doctor doc = doctors.stream().filter(doctor -> doctor.getId().equals(id)).findFirst().get();
 
-        return Optional.ofNullable(doctors.remove(doc.getId().intValue()));
+        return Optional.ofNullable(doctors.remove(doctors.indexOf(doc)));
     }
 }
